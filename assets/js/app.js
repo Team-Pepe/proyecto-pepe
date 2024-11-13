a934b8aa8acc2473c2d68ded01ad473d6bfd74bd
schnneider <schnneider.arias@utp.edu.co>
2024-11-13 16:37:39 -0500

    edicion de tareas

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,11 @@ const taskList = [];
 taskForm.addEventListener('submit', event => {
   event.preventDefault();
   
-  if(event.submitter.id === "clear") {
+  if (event.submitter.id === "clear") {
     taskForm.reset();
   }
   
-  if(event.submitter.id === "save") {
+  if (event.submitter.id === "save") {
     const task = {
       title: event.target.title.value,
       description: event.target.description.value,
@@ -16,31 +16,46 @@ taskForm.addEventListener('submit', event => {
       status: "active"
     };
 
-    // Agregar la tarea al arreglo taskList y guardarla en el localStorage
     taskList.push(task);
     localStorage.setItem('taskList', JSON.stringify(taskList));
     taskForm.reset();
-    
-    // Obtener las tareas del localStorage y volver a renderizarlas
-    const taskList_temp = JSON.parse(localStorage.getItem('taskList'));
-    const taskContainer = document.querySelector('#taskContainer');
-    
-    // Limpiar el contenido de taskContainer antes de renderizar las tareas
-    taskContainer.innerHTML = '';
-    
-    if(taskList_temp) {
-      taskList_temp.forEach((task, index) => {
-        taskContainer.innerHTML += `
-          <article>
-            <span>${index}</span>
-            <h3>${task.title}</h3>
-            <p>${task.description}</p>
-            <span>${task.status}</span>
-            <span>${task.category_description}</span>
-            <span>${task.category}</span>
-          </article>
-        `;
-      });
-    }
+    renderTasks();
   }
 });
+
+function renderTasks() {
+  const taskList_temp = JSON.parse(localStorage.getItem('taskList')) || [];
+  const taskContainer = document.querySelector('#taskContainer');
+  taskContainer.innerHTML = '';
+
+  taskList_temp.forEach((task, index) => {
+    taskContainer.innerHTML += `
+      <article id="task-${index}">
+        <span>${index}</span>
+        <h3 contenteditable="true">${task.title}</h3>
+        <p contenteditable="true">${task.description}</p>
+        <span>${task.status}</span>
+        <span>${task.category_description}</span>
+        <span>${task.category}</span>
+        <button onclick="saveEdits(${index})">Guardar Edición</button>
+      </article>
+    `;
+  });
+}
+
+function saveEdits(index) {
+  const taskContainer = document.querySelector(`#task-${index}`);
+  const editedTask = {
+    title: taskContainer.querySelector('h3').innerText,
+    description: taskContainer.querySelector('p').innerText,
+    category: taskList[index].category,
+    category_description: taskList[index].category_description,
+    status: taskList[index].status
+  };
+  
+  taskList[index] = editedTask;
+  localStorage.setItem('taskList', JSON.stringify(taskList));
+  renderTasks();
+}
+
+renderTasks();
